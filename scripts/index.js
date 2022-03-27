3d8e928e4969c5d24aa9ca1ee991d557e216928d
Jake <jacob37857@gmail.com>
2022-03-27 17:34:25 -0400

    init commit & starter functions

---
+++ scripts/index.js
@@ -0,0 +1,21 @@
+const add = (a, b) => {
+  return a + b;
+}
+
+const subtract = (a, b) => {
+  return a - b;
+}
+
+const multiply = (a, b) => {
+  return a * b;
+}
+
+const divide = (a, b) => {
+  return a / b;
+}
+
+
+console.log(add(2, 2)); // 4
+console.log(subtract(3, 2)); //1
+console.log(multiply(2, 4)); // 8 
+console.log(divide(4, 2)); //2
