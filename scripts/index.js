bad633afacfc0d9371092f70badcfdc9c3709a8b
Jacob Hoard <jacob37857@gmail.com>
2022-03-28 01:07:49 -0400

    fixed buttons to display on-screen

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,6 @@
+const display = document.querySelector('.display');
+const button = document.querySelectorAll('.btn');
+
 const add = (a, b) => {
   return a + b;
 }
@@ -32,8 +35,15 @@ const operate = (operator, num1, num2) => {
   }
 }
 
+const updateDisplay = (digit) => {
+  const value = digit.target.value;
+  display.textContent  = value;
+}
 
-console.log(add(2, 2)); // 4
-console.log(subtract(3, 2)); //1
-console.log(multiply(2, 4)); // 8 
-console.log(divide(4, 2)); //2
+button.forEach(btn => btn.addEventListener('click', updateDisplay));
+
+
+//console.log(add(2, 2)); // 4
+//console.log(subtract(3, 2)); // 1
+//console.log(multiply(2, 4)); // 8 
+//console.log(divide(4, 2)); // 2
